2963828ac38d8ae58c9ae3bf6c9caf89f4569b80
BigStar-2024 <bigstar2024skytech@gmail.com>
2024-05-16 08:07:59 -0500

    mobile useeffect fix

--- src/Pages/CryptoWallet/CryptoWalletTopCards/CryptoWalletTopCardsMobile.js
+++ src/Pages/CryptoWallet/CryptoWalletTopCards/CryptoWalletTopCardsMobile.js
@@ -24,7 +24,7 @@ import ComponentLoader from "../../../components/ProgressLoader/ComponentLoader"
 
 // Router
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MetaMaskSDK from "@metamask/sdk";
 
 new MetaMaskSDK({
